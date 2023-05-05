23702fbab17ae549f5e2d7d1d03524a05ad1a1e6
Teinonen Otto <otto.teinonen@cinia.fi>
2023-05-05 15:14:50 +0300

    Added a bunch of stuff

---
+++ release.ps1
@@ -0,0 +1,18 @@
+﻿$releases = "win-x64","win-x86","linux-x64","osx-x64"
+
+foreach($rid in $releases) {
+    dotnet publish `
+        -c Release `
+        -r $rid `
+        -p:PublishSingleFile=true `
+        -p:PublishTrimmed=true `
+        --self-contained true `
+        --output "releases/$rid"
+
+    $executable_path = ".\releases\${rid}\ps-gpt";
+    if ($rid.StartsWith("win")) {
+        $executable_path += ".exe";
+    }
+
+    Compress-Archive -Path $executable_path -DestinationPath ".\releases\$rid.zip" -Force
+}
